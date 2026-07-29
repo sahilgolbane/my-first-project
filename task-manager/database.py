310ce399f9d002d2aa650d347a7a76bb2c1fe6a0
sahil <sahilgolbane17@gmail.com>
2026-07-29 20:44:31 +0530

    debug print database url

--- task-manager/database.py
+++ task-manager/database.py
@@ -1,6 +1,8 @@
 import os
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:password@localhost/test_db")
+print("=== USING DATABASE URL:", DATABASE_URL, "===") 
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
