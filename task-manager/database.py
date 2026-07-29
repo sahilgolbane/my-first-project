ff84b72fcd74dd82adb3d026c16f218ab8ed0920
sahil <sahilgolbane17@gmail.com>
2026-07-30 00:56:26 +0530

    fix duplicate database url

--- task-manager/database.py
+++ task-manager/database.py
@@ -7,7 +7,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost/test_db"
 
 engine = create_engine(DATABASE_URL)
 
